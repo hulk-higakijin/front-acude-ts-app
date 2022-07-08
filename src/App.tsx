b7de07d54e5f6a2a916b04f6d9be7cd6cfe54770
hulk-higakijin <hm385.chejptks@gmail.com>
2022-07-09 01:14:47 +0900

    chore: typescriptの導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+
+// pages
+import { Home } from './pages/Home'
+import { Page404 } from './pages/Page404'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/*" element={<Page404 />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ src/pages/Page404.tsx
@@ -0,0 +1,8 @@
+export const Page404 = () => {
+  return (
+    <>
+      <h2>Page 404 Not Found...</h2>
+      <p>I'm sorry, but the page you're looking for doesn't exits.</p>
+    </>
+  )
+}
